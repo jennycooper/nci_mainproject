b3a68d4bd8ca1374f0b831a81929bdaeda2bb0fa
jennycooper <jennycooper@eircom.net>
2013-01-14 12:56:01 +0000

    change url link

--- app/models/OfflineCopy.java
+++ app/models/OfflineCopy.java
@@ -24,43 +24,40 @@ public class OfflineCopy {
 		BufferedWriter bw = null;
 		
 		try {
-			// get URL content
-			url = new URL("http://localhost:9000/download?year="+year);
-			URLConnection conn = url.openConnection();
-			// open the stream to the url and put it into BufferedReader
-			br = new BufferedReader(
-                               new InputStreamReader(conn.getInputStream()));
-			String inputLine;
-			//create a new directory (if it doesn't already exist) to store the offline copy
-			File dir = new File("/euaiki_offline_calendar");
-			if (!dir.exists()) {
-				dir.mkdir();
-			}
-			//save to this filename
-			File file = new File("/euaiki_offline_calendar/calendar_"+year+".html");
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-			
-			//use FileWriter to write file
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			bw = new BufferedWriter(fw);
-			
-			//write today's date to the start of the file
-			DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
-			// Get the date today using Calendar object.
-			Date today = Calendar.getInstance().getTime();        
-			String downloadDate = df.format(today);
-			String data =("<h2>" + "Copy downloaded on: "+ downloadDate + "</h2>");
-			bw.write(data);
+				// get URL content
+				url = new URL("https://damp-ravine-8097.herokuapp.com/download?year="+year);
+				URLConnection conn = url.openConnection();
+				// open the stream to the url and put it into BufferedReader
+				br = new BufferedReader(
+	                               new InputStreamReader(conn.getInputStream()));
+				String inputLine;
+				//create a new directory (if it doesn't already exist) to store the offline copy
+				File dir = new File("/euaiki_offline_calendar");
+				if (!dir.exists()) {
+					dir.mkdir();
+				}
+				//save to this filename
+				File file = new File("/euaiki_offline_calendar/calendar_"+year+".html");
+				if (!file.exists()) {
+					file.createNewFile();
+				}
+				
+				//use FileWriter to write file
+				FileWriter fw = new FileWriter(file.getAbsoluteFile());
+				bw = new BufferedWriter(fw);
+				
+				//write today's date to the start of the file
+				DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss");
+				// Get the date today using Calendar object.
+				Date today = Calendar.getInstance().getTime();        
+				String downloadDate = df.format(today);
+				String data =("<h2>" + "Copy downloaded on: "+ downloadDate + "</h2>");
+				bw.write(data);
+	
+				while ((inputLine = br.readLine()) != null) {
+					bw.write(inputLine);				
+				}
 
-			while ((inputLine = br.readLine()) != null) {
-				bw.write(inputLine);				
-			}
-
-			//bw.close();
-			//br.close();
-			System.out.println("Done");
  
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
